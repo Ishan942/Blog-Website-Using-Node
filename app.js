9c291cc7d9a267c484e154256153c42016014f2c
Ishan942 <ishansirdeshpande@gmail.com>
2023-08-08 18:12:45 +0530

    added database

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import express from "express";
 // import axios from "axios";
 import lodash from "lodash";
 import bodyParser from "body-parser";
-let posts = [];
+import mongoose from "mongoose";
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -13,50 +13,97 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
+async function startServer() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/postDB");
+  console.log("Connected to MongoDB");
+  const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home.ejs", {
-    content: homeStartingContent,
-    post_array: posts,
+  const PostSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+    body: String,
   });
-});
 
-app.get("/about", (req, res) => {
-  res.render("about.ejs", {
-    content: aboutContent,
+  const PostModel = mongoose.model("post", PostSchema);
+
+  const post1 = new PostModel({
+    title: "Post 1",
+    body: "This is post 1",
   });
-});
 
-app.get("/contact", (req, res) => {
-  res.render("contact.ejs", {
-    content: contactContent,
+  const post2 = new PostModel({
+    title: "Post 2",
+    body: "This is post 2",
   });
-});
 
-app.get("/compose", (req, res) => {
-  res.render("compose.ejs");
-});
-app.get("/post/:id", (req, res) => {
-  posts.forEach((post) => {
-    if (lodash.lowerCase(post.title) === lodash.lowerCase(req.params.id)) {
-      res.render("post.ejs", { post: post });
+  const post3 = new PostModel({
+    title: "Post 3",
+    body: "This is post 3",
+  });
+  const defaultArr = [post1, post2, post3];
+
+  app.get("/", async (req, res) => {
+    const posts = await PostModel.find();
+    if (posts.length === 0) {
+      await PostModel.insertMany(defaultArr);
+      res.redirect("/");
+    } else {
+      res.render("home.ejs", {
+        content: homeStartingContent,
+        post_array: posts,
+      });
     }
   });
-});
 
-app.post("/compose", (req, res) => {
-  const post = {
-    title: req.body.title,
-    body: req.body.body,
-  };
-  posts.push(post);
-  res.redirect("/");
-});
+  app.get("/about", (req, res) => {
+    res.render("about.ejs", {
+      content: aboutContent,
+    });
+  });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
+  app.get("/contact", (req, res) => {
+    res.render("contact.ejs", {
+      content: contactContent,
+    });
+  });
+
+  app.get("/compose", (req, res) => {
+    res.render("compose.ejs");
+  });
+  app.get("/post/:id", async (req, res) => {
+    const post = await PostModel.findOne({
+      title: req.params.id,
+    });
+    // posts.forEach((post) => {
+    //   if (lodash.lowerCase(post.title) === lodash.lowerCase(req.params.id)) {
+    //     res.render("post.ejs", { post: post });
+    //   }
+    // });
+
+    if (post) {
+      res.render("post.ejs", { post: post });
+    } else {
+      res.redirect("/");
+    }
+  });
+
+  app.post("/compose", async (req, res) => {
+    const new_post = new PostModel({
+      title: req.body.title,
+      body: req.body.body,
+    });
+    await new_post.save();
+    res.redirect("/");
+  });
+
+  app.listen(3000, function () {
+    console.log("Server started on port 3000");
+  });
+}
+
+startServer();
